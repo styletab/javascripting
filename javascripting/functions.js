48c43a75770046ab54c62671edb981072ad37bf3
styletab <katherinetab@gmail.com>
2016-08-27 22:23:36 -0400

    Functions
    FUNCTIONS (Exercise 17 of 19)
    
      A function is a block of code that takes input, processes that input, and
      then produces output.
    
      Here is an example:
    
         function example (x) {
           return x * 2;
         }
    
      We can call that function like this to get the number 10:
    
         example(5)
    
      The above example assumes that the example function will take a number as
      an argument –– as input –– and will return that number multiplied by 2.

---
+++ javascripting/functions.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let eat = function(food) {
+  return food + ' tasted really good.';
+};
+
+console.log(eat('bananas'));
+
+// when logging the result, make sure that the call includes '' for string
+// elements
